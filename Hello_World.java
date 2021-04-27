5cbc98997340e8384c9704a5f38aa8feea4c296f
root <root@ip-172-31-20-210.ec2.internal>
2021-04-27 08:51:33 +0000

    3 lines added to hello world

--- Hello_World.java
+++ Hello_World.java
@@ -1,5 +1,7 @@
 class Hello_World{  
     public static void main(String args[]){  
-     System.out.println("Hello World");  
+     System.out.println("Hello Guys"); 
+     System.out.println("Lets start learning");
+     System.out.println("------- DEVOPS --------");
     }  
 }  
